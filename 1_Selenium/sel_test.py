65ebe4c2d6136dfeb73bb1f9e7f09d158cb5d67d
Jamie Chambers <jamiechambers21@gmail.com>
2023-10-03 12:42:12 +0200

    Know how to use the mouse in Selenium

--- 1_Selenium/sel_test.py
+++ 1_Selenium/sel_test.py
@@ -7,30 +7,48 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Not necessary
-# PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome()
 
-driver.get("https://jqueryui.com/")
-print(driver.title)
-
-search = driver.find_element(by=By.NAME, value="s")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
-
-content = driver.find_element(by=By.ID, value="content")
-
-articles = content.find_elements(by=By.TAG_NAME, value="article")
-for article in articles:
-    header = article.find_element(by=By.CLASS_NAME, value="entry-title")
-    print(header.text)
 
 
+def use_search_bar():
+    driver.get("https://jqueryui.com/")
+    print(driver.title)
 
-# Wait that I´m not using 
-# wait = WebDriverWait(driver, timeout=2)
+    search = driver.find_element(by=By.NAME, value="s")
+    search.clear() # Clear search bar 
+    search.send_keys("test")
+    search.send_keys(Keys.RETURN)
 
-# wait.until(lambda d : content.is_displayed())
-# print(content.text)
+    content = driver.find_element(by=By.ID, value="content")
 
-time.sleep(20)
+    articles = content.find_elements(by=By.TAG_NAME, value="article")
+    for article in articles:
+        header = article.find_element(by=By.CLASS_NAME, value="entry-title")
+        print(header.text)
+
+def use_mouse():
+    driver.get("https://jqueryui.com/")
+    print(driver.title)
+    
+    link = driver.find_element(by=By.LINK_TEXT, value="Add Class")
+    link.click()
+
+    try:
+        wait = WebDriverWait(driver, timeout=5)
+
+        elem = wait.until(EC.presence_of_element_located((By.ID, "button")))
+        elem.click()
+
+        driver.back()
+        driver.back()
+        # driver.forward()
+
+    except:
+        driver.quit()
+
+# use_search_bar()
+use_mouse()
+
+
+time.sleep(5)
